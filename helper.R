a987f885fc459bdac993eae639eb35be38f0d529
klittle314 <klittle@iecodesign.com>
2020-03-29 23:54:17 -0500

    use the NA for 0 substitution rule divide the data into three chunks:  data for fitting the limits--the baseline, observed data beyond baseline and days beyond end of the series.

--- helper.R
+++ helper.R
@@ -33,52 +33,68 @@ make_country_data <- function(data,country_name,buffer_days,baseline){
  
   #per Provost discussion, simply add 1 to deaths uniformly:  
   #KL note:  I think setting value to NA for a zero in the initial series makes more sense.
-  #df1_X_deaths$deaths_nudge <- unlist(lapply(df1_X_deaths$deaths,zero_NA))
-  df1_X_deaths$deaths_nudge <- df1_X_deaths$deaths + 1
+  df1_X_deaths$deaths_nudge <- unlist(lapply(df1_X_deaths$deaths,zero_NA))
+  #df1_X_deaths$deaths_nudge <- df1_X_deaths$deaths + 1
   
   df1_X_deaths$log_count_deaths <- log10(df1_X_deaths$deaths_nudge)
   df1_X_deaths$serial_day <- c(1:nrow(df1_X_deaths))
   
-  data_use <- df1_X_deaths[df1_X_deaths$serial_day <= baseline,]
+  
+  data_use_cc <- df1_X_deaths[df1_X_deaths$serial_day <= baseline,]
   buffer <- buffer_days
   
   #create linear model
-  #now compute the linear regression for the log10 counts
-  lm_out <- lm(data=data_use,data_use$log_count_deaths ~ data_use$serial_day)
+  #now compute the linear regression for the log10 counts 
+  #lm_out <- lm(data=data_use_cc,log_count_deaths ~ serial_day)
+  lm_out <- lm(data=data_use_cc,data_use_cc$log_count_deaths ~ data_use_cc$serial_day)
   
-  #should handle the break in the series more elegantly
+  #should handle the break in the series more elegantly if we use the 'replace embedded 0 with NA" rule
   
-  cchart_df <- data.frame(data_use[!is.na(data_use$log_count_deaths),c("dateRep","serial_day","log_count_deaths")],
+  cchart_df <- data.frame(data_use_cc[!is.na(data_use_cc$log_count_deaths),c("dateRep","serial_day","log_count_deaths")],
                           lm_out$residuals,c(NA,diff(lm_out$residuals)),lm_out$fitted.values)
-  names(cchart_df)[5] <- "moving_range"
+  names(cchart_df)[5] <- "differences"
   
-  AvgMR <- mean(abs(cchart_df$lm_out.residuals))
-  cchart_df$UCL <- lm_out$fitted.values+2.66*mean(AvgMR,na.rm=TRUE)
-  cchart_df$LCL <- lm_out$fitted.values-2.66*mean(AvgMR,na.rm=TRUE)
+  AvgMR <- mean(abs(cchart_df$differences),na.rm=TRUE)
+  cchart_df$UCL <- lm_out$fitted.values + 2.66*AvgMR
+  cchart_df$LCL <- lm_out$fitted.values - 2.66*AvgMR
+  
   
   #buffer with buffer days beyond max date
-  buffer_serial_day <- seq(from=max(cchart_df$serial_day)+1,to=max(cchart_df$serial_day)+buffer,by=1)
+  buffer_serial_day <- seq(from=max(df1_X_deaths$serial_day)+1,to=max(df1_X_deaths$serial_day)+buffer,by=1)
   predicted_value <- lm_out$coefficients[1]+ lm_out$coefficients[2]*buffer_serial_day
-  buffer_dates <- seq.Date(from=max(cchart_df$dateRep)+1,to=max(cchart_df$dateRep)+buffer,by="day")
+  buffer_dates <- seq.Date(from=max(df1_X_deaths$dateRep)+1,to=max(df1_X_deaths$dateRep)+buffer,by="day")
   buffer_df <- cbind.data.frame(buffer_dates,
                                 buffer_serial_day,
                                 rep(NA,buffer),
                                 rep(NA,buffer),
                                 rep(NA,buffer),
                                 predicted_value,
-                                predicted_value +2.66*mean(AvgMR,na.rm=TRUE),
-                                predicted_value - 2.66*mean(AvgMR,na.rm=TRUE))
+                                predicted_value + 2.66*AvgMR,
+                                predicted_value - 2.66*AvgMR)
   
   names(buffer_df) <- names(cchart_df)
   
-  
-  df_cchart <- rbind(cchart_df,buffer_df)
+  #fill out the data table if not all observations are used to compute the control limits
+  df_check <- df1_X_deaths[df1_X_deaths$serial_day > baseline,]
+  if(!is.null(df_check)){
+    check_serial_day <- 
+    check_predicted_value <- lm_out$coefficients[1]+ lm_out$coefficients[2]*df_check$serial_day
+    
+    df_check_out <- cbind.data.frame(df_check[,c(1:3)],
+                                     rep(NA,nrow(df_check)),
+                                     rep(NA,nrow(df_check)),
+                                     check_predicted_value,
+                                     check_predicted_value + 2.66*AvgMR,
+                                     check_predicted_value - 2.66*AvgMR)
+    names(df_check_out) <- names(cchart_df)
+    df_cchart <- rbind(cchart_df,df_check_out,buffer_df)
+  } else df_cchart <- rbind(cchart_df,buffer_df)
   
   df_cchart$predict <- 10^df_cchart$lm_out.fitted.values
   df_cchart$UCL_anti_log <- 10^df_cchart$UCL
   df_cchart$LCL_anti_log <- 10^df_cchart$LCL
   
-  results_list <- list(data_use,df_cchart,lm_out)
+  results_list <- list(df1_X_deaths,df_cchart,lm_out)
   
 }
 
