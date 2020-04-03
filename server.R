a62b7967898750a6b0523486d239d19d96a0c633
klittle314 <klittle@iecodesign.com>
2020-04-02 23:06:31 -0500

    split the original control_chart reactive function into two reactive functions: 1. make_data()  creates the dfs and lm 2. control_chart() makes the charts The control_chart function now also makes the linear model chart (the logged values)
    Also, add output for the linear model chart

--- server.R
+++ server.R
@@ -114,20 +114,29 @@ shinyServer(function(input, output, session) {
                 as.character(Sys.Date()))
     })
     
+    #make a list that has a frame of the original data, a frame to construct the limit chart, and the linear model
+    make_data <- reactive({
+        location_use <- input$choose_location
+        data1 <- display_data()
+        buffer <- input$buffer
+        baseline1 <- input$baseline_n
+        start_date1 <- input$start_date
+        list_use <- make_location_data(data=data1,
+                                       location_name=location_use,
+                                       buffer_days=buffer,
+                                       baseline=baseline1,
+                                       start_date=start_date1)
+        
+        return(list_use)
+    })
+    
     control_chart <- reactive({
         location_use <- input$choose_location
         buffer <- input$buffer
         baseline1 <- input$baseline_n
-        start_date1 <- input$start_date
-        list_use <- make_location_data(data=display_data(),
-                                      location_name=location_use,
-                                      buffer_days=buffer,
-                                      baseline=baseline1,
-                                      start_date=start_date1)
-        data_use <- list_use[[1]]
-        df_cchart <- list_use[[2]]
-        lm_out <- list_use[[3]]
-        
+        data_use <- make_data()[[1]]
+        df_cchart <- make_data()[[2]]
+        #lm_out <- make_data()[[3]]
         p0 <- ggplot(data=data_use,aes(x=dateRep,y=deaths_nudge))+
             theme_bw()+
             geom_point(size=rel(3.0),colour="blue")+
@@ -150,12 +159,42 @@ shinyServer(function(input, output, session) {
             geom_line(data=df_cchart,aes(x=dateRep,y=UCL_anti_log),linetype="dotted")+
             geom_line(data=df_cchart,aes(x=dateRep,y=LCL_anti_log),linetype="dotted")
         
-        return(list(p3,df_cchart,lm_out))
+        #retrict to the values used in the linear fit to plot the log chart
+        df_cchart1 <- df_cchart %>% filter(serial_day <= baseline1)
+        
+        p_log <- ggplot(data=df_cchart1,aes(x=dateRep,y=log_count_deaths))+
+                theme_bw()+
+                geom_point(size=rel(2.5),colour="blue")+
+                geom_line()+
+                labs(title=paste0(location_use,"Log10 Daily New Deaths"),
+                     subtitle="Limits based on Individuals Shewhart chart calculations using regression residuals")+
+                ylab("Log10(Deaths)")+
+                xlab("")+
+                theme(axis.title.y=element_text(angle=0,vjust=0.5))+
+                geom_line(data=df_cchart1,aes(x=dateRep,y=lm_out.fitted.values))+
+                geom_line(data=df_cchart1,aes(x=dateRep,y=UCL),linetype="dotted")+
+                geom_line(data=df_cchart1,aes(x=dateRep,y=LCL),linetype="dotted")
+                
+                
+        
+        return(list(p0,p3,p_log))
         
     })
     
     output$control_chart <- renderPlot({
-        print(control_chart()[[1]])
+        #FIX REQUIRED 4/2/2020
+        #put conditional test:  plot just the data and message about short series, object control_chart()[[1]]
+        #if series OK, then display control_chart()[[2]]
+        #if no data, print message
+        print(control_chart()[[2]])
+    })
+    
+    output$log_control_chart <- renderPlot({
+        #FIX REQUIRED 4/2/2020
+        #put conditional test:  plot just the data and message about short series, object control_chart()[[1]]
+        #if series OK, then display control_chart()[[2]]
+        #if no data, print message
+        print(control_chart()[[3]])
     })
     
     output$download_chart <- downloadHandler(
@@ -171,8 +210,10 @@ shinyServer(function(input, output, session) {
     )
     
     output$data_table <- DT::renderDataTable({
-        req(control_chart())
-        df_out <- control_chart()[[2]][,c(1,2,3,9,11,10)]
+        req(make_data())
+       #df_out <- make_data()[[2]][,c(1,2,3,9,11,10)]
+        df_out <- make_data()[[2]][,c("dateRep","serial_day","log_count_deaths",
+                                      "predict","LCL_anti_log","UCL_anti_log")]
         names(df_out) <- c("Date Reported","Serial Day","Deaths","Predicted Deaths","Lower Limit","Upper Limit")
         df_out$Deaths <- 10^df_out$Deaths
         df_out$'Predicted Deaths' <- round(df_out$'Predicted Deaths',0)
@@ -184,9 +225,18 @@ shinyServer(function(input, output, session) {
     
     #add parameters to the calculations page
     output$parameters <- renderPrint({
-        req(control_chart())
-        intercept <- control_chart()[[3]][[1]][1]
+        req(make_data())
+        #require conditional check: if lm object NULL then print message no linear model fitted
+        #if lm_object used, then summarize the number of records used, the intercept and slope
+        #possibly can show the linear plot on the log scale
+        print("values from fitting a straight line by least squares to log10(deaths)")
+        intercept <- make_data()[[3]][[1]][1]
         print(intercept)
+        slope <- make_data()[[3]][[1]][2]
+        print(slope)
+        print("more stuff goes here")
+        
+        
     })
     
  })
